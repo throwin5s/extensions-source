7c7178cf7ada05d90b3daf9ecd37ede12e20aae4
throwin5s <throwin5s@gmail.com>
2026-06-28 18:06:05 -0700

    Comix Self Hosted: v1.4.34, add proguard keep rule for Descrambler

--- src/en/comixselfhosted/build.gradle.kts
+++ src/en/comixselfhosted/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Comix Self Hosted"
     className = "Comix"
-    versionCode = 33
+    versionCode = 34
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
 
